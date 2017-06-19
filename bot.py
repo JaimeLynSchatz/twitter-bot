a9555f78b1c734f009a0fcd5c90f5da4b2ee2933
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2017-06-19 13:34:36 -0700

    first try

---
+++ bot.py
@@ -0,0 +1,17 @@
+import tweepy, random
+from secrets import *
+
+auth = tweepy.OAuthHanlder(C_KEY, C_SECRET)
+auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+tweets = [
+	"Hello, World! Are you ready for this?",
+	"Nobody expects the Spanish Inquisition!",
+	"And now for something completely different.",
+	"Hey, Siri, what's the weather going to be like today? Whoops. Wrong app.",
+	"Goodnight, all. Yes, I know it's early, but it's really time."
+	]
+
+tweet_num = random.randint(0, length(tweets) -1 )
+api.update_status(tweets[tweet_num])
